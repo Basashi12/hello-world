12024c8b11bd2561765af35f99447c4b635e85d6
KT12 <nyy2008@gmail.com>
2017-01-31 23:15:12 -0500

    Scala

---
+++ hello_world.scala
@@ -0,0 +1,8 @@
+object HelloWorld {
+	/* First Scala program
+	* Will print 'Hello, world!' as the output
+	*/
+	def main(args: Array[string]) {
+		println("Hello, world!") // prints Hello, world!
+	}
+}
